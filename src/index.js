17a242fa0fdc32d8105232a031027c148bb92054
JonnattanChoque <jdevelopertwon@gmail.com>
2022-11-02 15:29:25 -0500

    Add firebase

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
+import "./utils/firebase";
 import "./scss/global.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/utils/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtUfdqdvP0GEUV6J1RLORMh68zGLDdatU",
+  authDomain: "musicfy-499a5.firebaseapp.com",
+  projectId: "musicfy-499a5",
+  storageBucket: "musicfy-499a5.appspot.com",
+  messagingSenderId: "1025657452671",
+  appId: "1:1025657452671:web:60ced54ba1082f21bba74a",
+  measurementId: "G-3W4L5E03ZS"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
