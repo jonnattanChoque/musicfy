211ffbc577cbc4142ad6e892f3dc00ad020135a0
JonnattanChoque <jdevelopertwon@gmail.com>
2022-11-02 23:11:46 -0500

    Login and handler error v1

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,5 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { FirebaseError } from "@firebase/util";
+import { getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from "firebase/auth";
 
 export class Auth {
     async registerUser(email, password) {
@@ -7,17 +8,51 @@ export class Auth {
             const result = await createUserWithEmailAndPassword(auth, email, password);
             return result;
         } catch (error) {
-            throw error;
+            errorHandler(error);
+        }
+    }
+
+    async login(email, password) {
+        try {
+            const auth = getAuth();
+            const result = await signInWithEmailAndPassword(auth, email, password);
+            return result;
+        } catch (error) {
+            errorHandler(error);
         }
     }
 
     async logout() {
         try {
             const auth = getAuth();
-            await auth.signOut();
+            await signOut(auth);
         } catch (error) {
-            throw error;
+            errorHandler(error);
         }
+    }
+}
+
+export function errorHandler(error) {
+    console.log(error.code);
+    var result = "";
+    if (error instanceof FirebaseError) {
+        switch (error.code) {
+            case 'auth/wrong-password':
+                result = 'Correo o contraseña incorrectos';
+                break;
+            case 'auth/user-not-found':
+                result = 'Correo o contraseña incorrectos';
+                break;
+            case 'auth/network-request-failed':
+                result = 'Error de conexión';
+                break;
+            default:
+                result = 'Error desconocido, intente más tarde';
+                break;
+        }
+        throw result;
+    } else {
+        throw error;
     }
 }
 
--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,14 +1,46 @@
-import React from 'react'
-import { Button } from 'semantic-ui-react'
+import React, {useState} from 'react'
+import { Form, Icon } from 'semantic-ui-react';
+import "./LoginForm.scss";
+import { useFormik } from 'formik';
+import { initialValues, validationSchema } from "./LoginForm.data";
+import { Auth } from '../../../api';
+
+const auth = new Auth();
 
 export function LoginForm(props) {
   const { openRegisterForm, goBack } = props;
-  return (
-    <div style={{backgroundColor: "#f3f3f3"}}>
-        <h1>Login Form</h1>
+  const [showPassword, setShowPassword] = useState(false)
 
-        <Button primary onClick={openRegisterForm}>Registro</Button>
-        <Button secondary onClick={goBack}>Atras</Button>
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formData) => {
+      try {
+        await auth.login(formData.email, formData.password);
+      } catch (error) {
+        alert(error);
+      }
+    }
+  });
+
+  return (
+    <div className='login-form'>
+      <h1>Música para todos</h1>
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Input type='text' placeholder="Correo" icon="mail outline" 
+          name="email" onChange={formik.handleChange} value={formik.values.email}
+          error={formik.errors.email}/>
+        <Form.Input type={showPassword ? 'text' : 'password'} placeholder="Contraseña" 
+          icon={<Icon name={showPassword ? 'eye slash' : 'eye'} link onClick={() => setShowPassword(!showPassword)} />} 
+          name="password" onChange={formik.handleChange} value={formik.values.password}
+          error={formik.errors.password}/>
+        <Form.Button type='submit' primary fluid loading={formik.isSubmitting}>Iniciar sesión</Form.Button>
+      </Form>
+      <div className='login-form__options'>
+        <p onClick={goBack}>Volver</p>
+        <p>¿No tienes cuenta? <span onClick={openRegisterForm}>Regístrate</span></p>
+      </div>
     </div>
   )
 }
